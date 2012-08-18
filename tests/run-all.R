e9d60b81041b691d297b53d30e22a6198adaf193
Emilio L. Cano <emilio.lopez@urjc.es>
2012-08-18 17:38:48 +0000

    version 0.6.2

---
+++ tests/run-all.R
@@ -0,0 +1,10 @@
+# Run all tests of the package
+# 
+# Author: Emilio L. Cano
+###############################################################################
+
+
+library(testthat)
+library(SixSigma)
+
+test_package("SixSigma")
